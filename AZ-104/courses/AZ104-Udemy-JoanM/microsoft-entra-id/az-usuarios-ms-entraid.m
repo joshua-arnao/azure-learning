205f66c4b009c8dd0d23f71e51e5706f8806a328
joshua-arnao <joshua.arnao@icloud.com>
2026-09-18 07:28:57 -0500

    new file about users in microssoft entra id

---
+++ AZ-104/courses/AZ104-Udemy-JoanM/microsoft-entra-id/az-usuarios-ms-entraid.m
@@ -0,0 +1,21 @@
+# CREACIÓN, CONFIGURACIÓN Y ADMINSITRACIÓN DE USUARIOS
+Cada usuario es igual a una cuenta de Azure para acceder a los recursos
+
+## Qué pasa al autenticarse con Entra ID:
+	- Se crea un token de acceso
+	- El token define a qué recursos puede entrar y que acciones puede realizar
+
+## Tipos de usuarios
+### Indetidades en la nube:
+- Existe solo en el di directorio de Micrsosof Entra
+- Se eliminan al quitarlos del directorio principal
+
+### Identidades sincronizadas por directorios:
+- Provienen de un servidor local como Active Directory Local
+- Se sincronizan mediante Miscrosoft Conect
+
+### Usuarios Invitados	
+- Cuentas externas de azure
+- Tienen acceso como usario invitados
+- pueden se eliminados junto con su cuenta y permisos
+	
